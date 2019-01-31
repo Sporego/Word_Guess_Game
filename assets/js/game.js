978e953af3785bf868a750d6f08e5bf8236bcada
Sporego <Hello@Filbert.me>
2019-01-31 17:45:17 -0500

    Defined prototype variables

---
+++ assets/js/game.js
@@ -0,0 +1,25 @@
+// Word List
+var wordList = [
+  "PLACEHOLDER 1",
+  "PLACEHOLDER 1",
+  "PLACEHOLDER 1",
+  "PLACEHOLDER 1",
+  "PLACEHOLDER 1"
+];
+
+// Lives
+var tryLimit = 10;
+// Tracks Guessed Letters
+var guessLibrary = [];
+// Tracks Current Word Index
+var wordIndex;
+// Lives Left
+var lives = 0;
+// Win Counter
+var wins = 0;
+// Loss Counter
+var losses = 0;
+// Did game start?
+var gameStarted = false;
+// Did game end?
+var gameFinished = false;
